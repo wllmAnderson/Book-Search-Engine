2a4c6c8e6aee356a97040d6e7328ef2cad412343
William Anderson <114448677+wllmAnderson@users.noreply.github.com>
2023-04-24 11:04:39 -0400

    apollo

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -4,12 +4,34 @@ const db = require('./config/connection');
 const routes = require('./routes');
 const graphqlHTTP = require("express-graphql")
 const buildSchema = require("graphql")
-
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schema');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+async function startApolloServer() {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  app.use((req, res) => {
+    res.status(200);
+    res.send('Hello!');
+    res.end();
+  });
+
+  await new Promise(resolve => app.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  return { server, app };
+}
 
 // if we're in production, serve client/build as static assets
 var schema = buildSchema(`
